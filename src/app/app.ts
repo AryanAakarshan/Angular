77df0c14224fb3473365d17d3731f7dc52d12ee9
AryanAakarshan <aryanaakarshan00@gmail.com>
2026-03-18 11:02:38 +0530

    Commiting

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-tutorial');
+  name="Aryan";
+  age=22;
+  hi="Tell me about it";
+  int=0;
+  
+}
