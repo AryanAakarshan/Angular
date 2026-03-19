ce139769d2fb27a9a5169c1f6007f745d3d6db72
AryanAakarshan <aryanaakarshan00@gmail.com>
2026-03-20 00:55:54 +0530

    Added getset tutorial

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,16 @@ import { every } from 'rxjs';
   styleUrl: './app.css'
 })
 export class App {
+  value: string='about';
+  items = ['about', 'contact', 'home'];
+  users=[
+  {name:'John', age:30},
+  {name:'Jane', age:25},
+  {name:'Doe', age:35}
+  ]
+  fruits=['apple', 'banana', 'cherry'];
+  delete(i:number){
+  this.fruits.splice(i,1);
+  }
 
 }
